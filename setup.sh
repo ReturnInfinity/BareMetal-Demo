50459c54d6c754610de369d3e15c3e12365b6162
Ian Seyler <ian@seyler.me>
2020-01-30 13:30:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 
 cd src
 wget -q https://github.com/ReturnInfinity/BareMetal/raw/master/api/libBareMetal.asm 
+wget -q https://github.com/ReturnInfinity/BareMetal/raw/master/api/libBareMetal.c 
+wget -q https://github.com/ReturnInfinity/BareMetal/raw/master/api/libBareMetal.h 
 cd ..
 
 mkdir bin
