25958c964dd156634e0173e3d19fff5b485d4fea
Ian Seyler <ian@seyler.me>
2020-02-12 17:45:01 -0500

    Build C examples

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ cd src
 nasm hello.asm -o ../bin/hello.app -l ../bin/hello-debug.txt
 nasm sysinfo.asm -o ../bin/sysinfo.app -l ../bin/sysinfo-debug.txt
 nasm counter.asm -o ../bin/counter.app
-
+gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o libBareMetal.o libBareMetal.c
+gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o helloc.o helloc.c
+ld -T c.ld -o ../bin/helloc.app helloc.o libBareMetal.o
+gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o graphics.o graphics.c
+ld -T c.ld -o ../bin/graphics.app graphics.o
 cd ..
