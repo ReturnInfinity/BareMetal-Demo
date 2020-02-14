4024a75cd4145517adaae13225e692d5b3cec961
Ian Seyler <ian@seyler.me>
2020-02-13 21:59:33 -0500

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -14,7 +14,7 @@ uint8_t depth;
 
 int main(void)
 {
-	// Gather video memory address, x & y resoultion, and BPP 
+	// Gather video memory address, x & y resoultion, and BPP
 	frame_buffer = (unsigned char *)((uint64_t)(*(uint32_t *)(0x5080)));
 	x_res = *(uint16_t *)(0x5084);
 	y_res = *(uint16_t *)(0x5086);
@@ -28,7 +28,7 @@ int main(void)
 	drawline(100, 100, 100, 200, 0xFF, 0xFF, 0xFF);
 	drawline(100, 200, 200, 200, 0xFF, 0xFF, 0xFF);
 	drawline(200, 200, 200, 100, 0xFF, 0xFF, 0xFF);
-	drawline(100, 100, 200, 100, 0xFF, 0xFF, 0xFF);
+	drawline(200, 100, 100, 100, 0xFF, 0xFF, 0xFF);
 
 }
 
@@ -42,25 +42,90 @@ void putpixel(int x, int y, char red, char green, char blue)
 
 void drawline(int x0, int y0, int x1, int y1, char red, char green, char blue)
 {
-	int dx, dy, p, x, y;
-	dx = x1 - x0;
-	dy = y1 - y0;
-	x = x0;
-	y = y0;
-	p = 2 * dy - dx;
-	while (x < x1)
-	{
-		if (p >= 0)
+	// Shamelessly adapted from https://stackoverflow.com/questions/10060046/drawing-lines-with-bresenhams-line-algorithm
+	int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
+        dx = x1 - x0;
+        dy = y1 - y0;
+        dx1 = dx;
+        dy1 = dy;
+	if (dx1 < 0) // Poor mans abs()
+		dx1 = dx1 - dx1 - dx1;
+	if (dy1 < 0) // Poor mans abs()
+		dy1 = dy1 - dy1 - dy1;
+        px = 2 * dy1 - dx1;
+        py = 2 * dx1 - dy1;
+        if(dy1 <= dx1)
+        {
+		if(dx >= 0)
 		{
-			putpixel(x, y, red, green, blue);
-			y += 1;
-			p = p + 2 * dy - 2 * dx;
+			x = x0;
+			y = y0;
+			xe = x1;
 		}
 		else
 		{
-			putpixel(x, y, red, green, blue);
-			p = p + 2 * dy;
+			x = x1;
+			y = y1;
+			xe = x0;
 		}
-		x += 1;
+		putpixel(x, y, red, green, blue);
+		for(i=0; x<xe; i++)
+		{
+			x = x + 1;
+			if(px < 0)
+			{
+				px = px + 2 * dy1;
+			}
+			else
+			{
+				if((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+				{
+					y = y + 1;
+				}
+				else
+				{
+					y = y - 1;
+				}
+				px = px + 2 * (dy1 - dx1);
+			}
+			putpixel(x, y, red, green, blue);
+		}
+	}
+	else
+	{
+		if(dy >= 0)
+		{
+			x = x0;
+			y = y0;
+			ye = y1;
+		}
+		else
+		{
+			x = x1;
+			y = y1;
+			ye = y0;
+		}
+		putpixel(x, y, red, green, blue);
+		for(i=0; y<ye; i++)
+		{
+			y = y + 1;
+			if(py <= 0)
+			{
+				py = py + 2 * dx1;
+			}
+			else
+			{
+				if((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+				{
+					x = x + 1;
+				}
+				else
+				{
+					x = x - 1;
+				}
+				py = py + 2 * (dx1 - dy1);
+			}
+			putpixel(x, y, red, green, blue);
+		}
 	}
 }
