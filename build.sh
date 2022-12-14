086b97bc7d0de6b1581a8d8b8fddf296d644b220
Ian Seyler <ian@ians-macbook-air.local>
2022-12-14 01:43:46 +0000

    Skip building C programs if running on macOS

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ nasm hello.asm -o ../bin/hello.app -l ../bin/hello-debug.txt
 nasm sysinfo.asm -o ../bin/sysinfo.app -l ../bin/sysinfo-debug.txt
 nasm counter.asm -o ../bin/counter.app
 nasm ethtest.asm -o ../bin/ethtest.app
-gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o libBareMetal.o libBareMetal.c
-gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o helloc.o helloc.c
-ld -T c.ld -o ../bin/helloc.app helloc.o libBareMetal.o
-gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o graphics.o graphics.c
-ld -T c.ld -o ../bin/graphics.app graphics.o
-gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o gavare.o gavare.c
-ld -T c.ld -o ../bin/gavare.app gavare.o
+if [ "$(uname)" != "Darwin" ]; then
+	gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o libBareMetal.o libBareMetal.c
+	gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o helloc.o helloc.c
+	ld -T c.ld -o ../bin/helloc.app helloc.o libBareMetal.o
+	gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o graphics.o graphics.c
+	ld -T c.ld -o ../bin/graphics.app graphics.o
+	gcc -c -m64 -nostdlib -nostartfiles -nodefaultlibs -o gavare.o gavare.c
+	ld -T c.ld -o ../bin/gavare.app gavare.o
+fi
 cd ..
