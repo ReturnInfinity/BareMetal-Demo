aa0812faebde0af61bf57f33fd6455a48d5d47a9
Ian Seyler <ian@seyler.me>
2023-10-24 16:00:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 ./clean.sh
 
 cd src
-if which curl &> /dev/null; then
+if [ command -v curl >/dev/null 2>&1 ]; then
 	curl -s -o libBareMetal.asm https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
 	curl -s -o libBareMetal.c https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
 	curl -s -o libBareMetal.h https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
 else
-	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
-	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
-	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
+	wget -q https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+	wget -q https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
+	wget -q https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
 fi
 cd ..
 
