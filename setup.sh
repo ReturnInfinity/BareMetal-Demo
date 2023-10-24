8269083aaa34315064e4d3e4adb79776201281c8
Ian Seyler <ian@ians-macbook-air.local>
2023-10-24 15:22:29 -0400

    Update setup to use curl or wget

--- setup.sh
+++ setup.sh
@@ -3,9 +3,15 @@
 ./clean.sh
 
 cd src
-curl -s -o libBareMetal.asm https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
-curl -s -o libBareMetal.c https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
-curl -s -o libBareMetal.h https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
+if which curl &> /dev/null; then
+	curl -s -o libBareMetal.asm https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+	curl -s -o libBareMetal.c https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
+	curl -s -o libBareMetal.h https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
+else
+	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.c
+	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.h
+fi
 cd ..
 
 mkdir bin
