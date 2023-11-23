00d4b2478b9494656f1f434b7b643b56a4a4f34e
Ian <ian@ians-mac-pro.local>
2023-11-22 20:26:03 -0500

    Use RIP-relative addressing

--- src/euler1.asm
+++ src/euler1.asm
@@ -13,44 +13,44 @@ start:					; Start of program label
 
 loop:
 	; Check if RDX is divisible by 3
-	mov rax, qword [counter]	; Copy counter to RAX for division
+	mov rax, qword [rel counter]	; Copy counter to RAX for division
 	xor rdx, rdx			; Zero out RDX for division
 	mov rcx, 3
 	div rcx				; Divide RDX:RAX by 3, the result is in RAX, the remainder in RDX
 	cmp rdx, 0			; Compare the remainder with 0
 	jne check_5			; If not divisible by 3, jump to check_5
-	mov rax, qword [counter]
-	add qword [sum], rax
+	mov rax, qword [rel counter]
+	add qword [rel sum], rax
 	jmp add_counter			; Jump to add_counter
 
 	; Check if RDX is divisible by 5
 check_5:
-	mov rax, qword [counter]	; Copy RDX to RAX for division
+	mov rax, qword [rel counter]	; Copy RDX to RAX for division
 	xor rdx, rdx			; Zero out RDX for division
 	mov rcx, 5
 	div rcx				; Divide RDX:RAX by 5, the result is in RAX, the remainder in RDX
 	cmp rdx, 0			; Compare the remainder with 0
 	jne add_counter			; If not divisible by 5, jump to add_counter
-	mov rax, qword [counter]
-	add qword [sum], rax
+	mov rax, qword [rel counter]
+	add qword [rel sum], rax
 
 	; Add 1 to the counter and check if it's less than 1000
 add_counter:
-	add qword [counter], 1		; Increment the counter
-	cmp qword [counter], 1000	; Compare the counter with 1000
+	add qword [rel counter], 1	; Increment the counter
+	cmp qword [rel counter], 1000	; Compare the counter with 1000
 	jl loop				; If less, jump to loop
 
 	mov rsi, message
 	call output
 	mov rdi, tstring
 	mov rsi, rdi
-	mov rax, qword [sum]		; Copy sum to RAX
+	mov rax, qword [rel sum]	; Copy sum to RAX
 	call int_to_string		; Convert value to RAX to string in [RDI]
 	call output
 	mov rsi, newline
 	call output
 
-ret				; Return to OS
+	ret				; Return to OS
 
 message: db 'Sum of multiples of 3 or 5 below 1000: ', 0
 sum: dq 0
