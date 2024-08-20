349d39970f5dc10cbee53f0b10e785c3f777dee3
Ian Seyler <ian@mini.local>
2024-08-20 14:58:19 -0400

    Add network functions to `systest`

--- src/systest.asm
+++ src/systest.asm
@@ -28,6 +28,8 @@ systest_wait_for_input:
 	jmp systest_wait_for_input
 
 systest_smp:
+	mov rsi, smpteststring
+	call output
 	mov rcx, smp_set		; API Code
 	mov rax, smp_task		; Code for CPU to run
 	xor edx, edx			; Start at ID 0
@@ -82,7 +84,110 @@ systest_mem_done:
 	jmp start
 
 systest_net:
-	jmp systest_wait_for_input
+	mov rsi, netteststring
+	call output
+
+	; Get the host MAC
+	mov rax, [0x110048]		; TODO Get from kernel via API
+	mov rdi, source
+	mov rcx, 6
+systest_net_srcmacnext:
+	stosb
+	shr rax, 8
+	sub rcx, 1
+	cmp rcx, 0
+	jne systest_net_srcmacnext
+
+systest_net_main:
+	mov rdi, buffer
+	call [b_net_rx]
+	cmp cx, 0
+	jne systest_net_receive
+	call [b_input]
+	or al, 00100000b		; Convert to lowercase
+	cmp al, 's'
+	je systest_net_send
+	cmp al, 'q'
+	je systest_net_finish
+	jmp systest_net_main
+
+systest_net_finish:
+	jmp start
+
+systest_net_send:
+	mov rsi, nettestsendstring
+	call output
+	mov rsi, packet
+	mov rcx, 64
+	call [b_net_tx]
+	jmp systest_net_main
+
+systest_net_receive:
+	mov rsi, nettestreceivestring
+	call output
+	mov rsi, buffer
+
+	; Output the destination MAC
+	mov rcx, 6
+systest_net_receive_dest:	
+	lodsb
+	call dump_al
+	sub rcx, 1
+	cmp rcx, 0
+	jne systest_net_receive_dest
+	push rsi
+	mov rsi, space
+	call output
+	pop rsi
+
+	; Output the source MAC
+	mov rcx, 6
+systest_net_receive_src:
+	lodsb
+	call dump_al
+	sub rcx, 1
+	cmp rcx, 0
+	jne systest_net_receive_src
+	push rsi
+	mov rsi, space
+	call output
+	pop rsi
+
+	; Output the EtherType
+systest_net_receive_type:
+	lodsb
+	mov bl, al
+	shl bx, 8
+	call dump_al
+	lodsb
+	mov bl, al
+	call dump_al
+	push rsi
+	mov rsi, space
+	call output
+	pop rsi
+	cmp bx, 0x0806
+	je systest_net_receive_type_arp
+	cmp bx, 0x0800
+	je systest_net_receive_type_ipv4
+	cmp bx, 0x86DD
+	je systest_net_receive_type_ipv6
+	jmp systest_net_main
+
+systest_net_receive_type_arp:
+	mov rsi, nettestARP
+	call output
+	jmp systest_net_main
+
+systest_net_receive_type_ipv4:
+	mov rsi, nettestIPv4
+	call output
+	jmp systest_net_main
+
+systest_net_receive_type_ipv6:
+	mov rsi, nettestIPv6
+	call output
+	jmp systest_net_main
 
 systest_end:
 	ret
@@ -197,15 +302,35 @@ dump_al:
 ; -----------------------------------------------------------------------------
 
 
-
+; Strings
 startstring: db 13, 'SysTest', 13, '========', 13, '1 - SMP Test', 13, '2 - Memory Test', 13, '3 - Network Test', 13, 'q - Quit', 13, 'Enter selection: ', 0
+smpteststring: db 13, 'SMP Test', 13, 'A message from each core should be displayed', 0
 smptestmessage: db 13, 'Hello from core 0x'
-memteststring: db 13, 'mem test', 13, 'starting at 0x', 0
+memteststring: db 13, 'Memory Test', 13, 'Starting at 0x', 0
 memteststring2: db ', testing up to ', 0
 memtesterror: db 13, 'Error at ', 0
-memtesterror2: db 13, 'Ending test', 0
+memtesterror2: db 13, 'Ending test early', 0
+netteststring: db 13, 'Network Test', 13, 'Press S to send a packet, Q to quit.', 13, 'Received packets will display automatically', 0
+nettestsendstring: db 13, 'Sending packet.', 0
+nettestreceivestring: db 13, 'Received packet: ', 0
+nettestIPv4: db 'IPv4', 0
+nettestARP: db 'ARP ', 0
+nettestIPv6: db 'IPv6', 0
 donestring: db 13, 'Done!', 13, 0
 hextable: db '0123456789ABCDEF'
+space: db ' ', 0
 newline: db 13, 0
 outputlock: dq 0
 tchar: db 0, 0, 0
+
+align 16
+
+packet:
+destination: db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+source: db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+type: db 0xAB, 0xBA
+data: db 'This is test data from EthTest for BareMetal'
+
+align 16
+
+buffer: db 0
