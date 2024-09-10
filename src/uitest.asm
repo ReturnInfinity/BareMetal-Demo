304a1e7c7e7848b49875077cffed28537240061d
Ian Seyler <ian@mini.local>
2024-09-10 16:28:33 -0400

    UI API update

--- src/uitest.asm
+++ src/uitest.asm
@@ -13,7 +13,7 @@ start:					; Start of program label
 	call [b_output]			; Print the string that RSI points to
 
 	mov eax, 0x00FF0000		; Red
-	mov cl, 1
+	mov cl, 0x11
 	call [b_user]
 	lea rsi, [rel test_message]	; Load RSI with the relative memory address of string
 	mov ecx, 4			; Output 14 characters
@@ -24,7 +24,7 @@ start:					; Start of program label
 	call [b_output]			; Print the string that RSI points to
 	
 	mov eax, 0x0000FF00		; Green
-	mov cl, 1
+	mov cl, 0x11
 	call [b_user]
 	lea rsi, [rel test_message]	; Load RSI with the relative memory address of string
 	mov ecx, 4			; Output 14 characters
@@ -35,37 +35,44 @@ start:					; Start of program label
 	call [b_output]			; Print the string that RSI points to
 	
 	mov eax, 0x000000FF		; Blue
-	mov cl, 1
+	mov cl, 0x11
 	call [b_user]
 	lea rsi, [rel test_message]	; Load RSI with the relative memory address of string
 	mov ecx, 4			; Output 14 characters
 	call [b_output]			; Print the string that RSI points to
 
 	mov eax, 0x00FF00FF
-	mov cl, 1
+	mov cl, 0x11
 	call [b_user]
 	mov eax, 0x0000FF00		; Green
-	mov cl, 2
+	mov cl, 0x12
 	call [b_user]
 	mov ax, 20
-	mov cl, 3
+	mov cl, 0x13
 	call [b_user]
 	mov ax, 10
-	mov cl, 4
+	mov cl, 0x14
 	call [b_user]
 	lea rsi, [rel test_message]	; Load RSI with the relative memory address of string
 	mov ecx, 4			; Output 14 characters
 	call [b_output]			; Print the string that RSI points to
 
+;fill:
+;	lea rsi, [rel string]		; Load RSI with the relative memory address of string
+;	mov ecx, 10			; Output 14 characters
+;	call [b_output]			; Print the string that RSI points to
+;	jmp fill
+
 	; Set colors back to default
 	mov eax, 0x00FFFFFF		; White
-	mov cl, 1
+	mov cl, 0x11
 	call [b_user]
 	mov eax, 0x00404040		; Dark grey
-	mov cl, 2
+	mov cl, 0x12
 	call [b_user]
 
 	ret				; Return to OS
 
 newline: db 10
 test_message: db 'TEST', 0
+string: db '0123456789', 0
