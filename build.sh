de3b171b167d5af03cdb5151ad661f71a3980934
Ian Seyler <ian@ians-macbook-air.local>
2025-05-25 12:06:17 -0400

    Add aditional CFLAGS for raytrace - This is for optimizing the program - -ftree-slp-vectorize is not included due to crashing

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CFLAGS="-c -m64 -nostdlib -nostartfiles -nodefaultlibs -ffreestanding -falign-functions=16 -fomit-frame-pointer -mno-red-zone -fno-builtin"
+CFLAGS_RAY="-O1 -falign-jumps=16 -falign-labels=16 -falign-loops=16 -fcaller-saves -fcode-hoisting -fcrossjumping -fcse-follow-jumps -fcse-skip-blocks -fdelete-null-pointer-checks -fdevirtualize -fdevirtualize-speculatively -fexpensive-optimizations -ffinite-loops -fgcse -fgcse-lm -fhoist-adjacent-loads -finline-functions -finline-small-functions -findirect-inlining -fipa-bit-cp -fipa-cp -fipa-icf -fipa-ra -fipa-sra -fipa-vrp -fisolate-erroneous-paths-dereference -flra-remat -foptimize-sibling-calls -foptimize-strlen -fpartial-inlining -fpeephole2 -freorder-blocks-algorithm=stc -freorder-blocks-and-partition -freorder-functions -frerun-cse-after-loop -fschedule-insns -fschedule-insns2 -fsched-interblock -fsched-spec -fstore-merging -fstrict-aliasing -fthread-jumps -ftree-builtin-call-dce -ftree-loop-vectorize -ftree-pre -ftree-switch-conversion -ftree-tail-merge -ftree-vrp -fvect-cost-model=very-cheap -fgcse-after-reload -fipa-cp-clone -floop-interchange -floop-unroll-and-jam -fpeel-loops -fpredictive-commoning -fsplit-loops -fsplit-paths -ftree-loop-distribution -ftree-partial-pre -funswitch-loops -fvect-cost-model=dynamic -fversion-loops-for-strides"
 
 cd src
 nasm hello.asm -o ../bin/hello.app -l ../bin/hello-debug.txt
@@ -18,7 +19,7 @@ if [ "$(uname)" != "Darwin" ]; then
 	ld -T c.ld -o ../bin/helloc.app crt0.o helloc.o libBareMetal.o
 	gcc $CFLAGS -o uitestc.o uitestc.c
 	ld -T c.ld -o ../bin/uitestc.app crt0.o uitestc.o libBareMetal.o
-	gcc $CFLAGS -o raytrace.o raytrace.c
+	gcc $CFLAGS $CFLAGS_RAY -o raytrace.o raytrace.c
 	ld -T c.ld -o ../bin/raytrace.app crt0.o raytrace.o libBareMetal.o
 	gcc $CFLAGS -o gavare.o gavare.c
 	ld -T c.ld -o ../bin/gavare.app crt0.o gavare.o libBareMetal.o
