b47e0bc02e058341014bd03c83d2b6b6f278747e
Ian Seyler <ian@seyler.me>
2026-03-10 11:10:18 -0400

    Refactor _start function for stack alignment

--- src/crt0.c
+++ src/crt0.c
@@ -5,7 +5,25 @@ extern char __bss_stop;
 
 static void zero_bss(void);
 
-int _start()
+/*
+ * Ensure RSP is 16-byte aligned. SSE instructions such as
+ * MOVAPS will #GP on the mis-aligned stack.
+ */
+__attribute__((naked)) void _start(void)
+{
+	__asm__ volatile (
+		"pushq %%rbp\n\t"        /* save rbp (callee-saved)     */
+		"movq %%rsp, %%rbp\n\t"  /* remember original RSP       */
+		"andq $-16, %%rsp\n\t"   /* ensure 16-byte alignment    */
+		"call _start_c\n\t"      /* CALL so RSP is 8-mod-16 inside _start_c */
+		"movq %%rbp, %%rsp\n\t"  /* restore original RSP        */
+		"popq %%rbp\n\t"         /* restore rbp                 */
+		"ret\n\t"                /* return to BareMetal OS       */
+		::: "memory"
+	);
+}
+
+int _start_c()
 {
 	zero_bss();
 
